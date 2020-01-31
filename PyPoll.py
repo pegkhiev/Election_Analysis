680b7b0408869615eca399fd0b86711fb3a21faa
Peggy Yang <pegyang@Peggys-Air.lan1>
2020-01-30 21:27:22 -0800

    updated py file 013020

--- PyPoll.py
+++ PyPoll.py
@@ -16,16 +16,57 @@ file_to_save = os.path.join("analysis", "election_analysis.txt")
 #1 Initialize total_vote accumulator as zero 
 total_votes = 0
 
+#Candidate options as list 
+candidate_options = []
+
+#Declare an empty dictionary for candidates_respective votes 
+candidate_votes = {}
+
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 #Read the file object with the reader function 
-with open(file_to_load) as election_data:
+with open(file_to_load, newline='') as election_data:
     file_reader = csv.reader(election_data)
     #read and print the header row 
     headers = next(file_reader)
     for row in file_reader:
         #2 add to total vote count
         total_votes+=1 
-#3 print total votes 
-print(total_votes)
+        # print candidate name from each row 
+        candidate_name = row[2]
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+            candidate_votes[candidate_name] = 0
+        candidate_votes[candidate_name]+=1
+
+for candidate_name in candidate_votes: 
+    votes = candidate_votes[candidate_name]
+    vote_percentage = votes/total_votes*100
+    
+
+    if (votes>winning_count) and (vote_percentage>winning_percentage):
+        winning_count = votes
+        winning_percentage = vote_percentage
+        winning_candidate = candidate_name
+
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})")      
+
+winning_candidate_summary = (f"-------------------\n" 
+f"Winner: {winning_candidate}\n" 
+f"Winning Vote Count: {winning_count:,}\n" 
+f"Winning Percentage: {winning_percentage:.1f}\n" 
+f"--------------------")
+
+print(winning_candidate_summary)
+
+
+
+
+
+
+
 
 
         
